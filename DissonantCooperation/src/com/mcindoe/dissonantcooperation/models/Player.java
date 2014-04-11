34864124cc1b15c67e1bc5f4f830b2d542508af1
rmcindoe3 <rmcindoe3@gatech.edu>
2014-04-11 18:57:11 -0400

    added xdel, ydel to player

--- DissonantCooperation/src/com/mcindoe/dissonantcooperation/models/Player.java
+++ DissonantCooperation/src/com/mcindoe/dissonantcooperation/models/Player.java
@@ -1,8 +1,30 @@
 package com.mcindoe.dissonantcooperation.models;
 
 public class Player extends GameObject {
+	
+	private int xdel, ydel;
 
 	public Player(int x, int y, int height, int width) {
 		super(x,y,height,width);
+		setXdel(0);
+		setYdel(0);
 	}
+
+	public int getXdel() {
+		return xdel;
+	}
+
+	public void setXdel(int xdel) {
+		this.xdel = xdel;
+	}
+
+	public int getYdel() {
+		return ydel;
+	}
+
+	public void setYdel(int ydel) {
+		this.ydel = ydel;
+	}
+	
+	
 }
